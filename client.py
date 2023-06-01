99a08bc9c977b1e7ec9370e6e8dedbce0cdfff6f
Eduardo Henrique <edu.ed322+git@gmail.com>
2023-06-01 15:25:35 -0300

    melhoria na escrita

--- client.py
+++ client.py
@@ -4,6 +4,7 @@ from collections import Counter
 from os import environ, walk
 import sys
 from dotenv import load
+from typing import List, Type
 
 from RepeatedTimer import RepeatedTimer
 
@@ -11,27 +12,31 @@ load()
 folder = sys.argv[1]
 
 
-def get_rarest_first(file_list):
+def get_client_files_name() -> Type[List[str]]:
+    return next(walk(folder), (List[str], None, []))[2]
+
+
+def get_rarest_first(peers):
     all_files = []
-    for client in file_list:
-        all_files += client[1]
+    for _, files in peers:
+        all_files += files
     counter = Counter(all_files)
-    my_files = next(walk(folder), (None, None, []))[2]
+    my_files = get_client_files_name()
     for i in my_files:
         del counter[i]
     order = sorted(counter, key=counter.get)
     while len(order) > 0:
         rarest = order.pop(0)
-        for client in file_list:
-            if rarest in client[1]:
-                print(client[0], rarest)
+        for peer, files in peers:
+            if rarest in files:
+                print(peer, rarest)
                 break
 
 
 def client_server_connection(**kwargs):
     c_socket = kwargs.get("socket")
 
-    filenames = next(walk(folder), (None, None, []))[2]
+    filenames = get_client_files_name()
     jsondump = json.dumps({"files": filenames})
     c_socket.send(jsondump.encode())
     data = c_socket.recv(1024).decode()
@@ -40,13 +45,13 @@ def client_server_connection(**kwargs):
 
 
 def client_program():
-    host = "192.168.100.6"  # as both code is running on same pc
+    host = "192.168.100.6"
     port = int(environ.get("SERVER_PORT") or 4000)
 
-    client_socket = socket.socket()  # instantiate
-    client_socket.connect((host, port))  # connect to the server
+    client_socket = socket.socket()
+    client_socket.connect((host, port))
     client_server_connection(socket=client_socket)
-    RepeatedTimer(10, client_server_connection, socket=client_socket)
+    RepeatedTimer(10, client_server_connection, socket=client_socket)  # connection server
     # message = input(" -> ")  # take input
     #
     # client_socket.close()  # close the connection
